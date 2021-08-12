289ee1afd2d995a74b44e5d9518eb053089fd669
Daniel Waite <danielwaite6@hotmail.com>
2021-08-12 01:04:20 -0300

    Hello Go Finances

---
+++ src/screens/CategorySelect/styles.ts
@@ -0,0 +1,37 @@
+import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFValue(113)}px;
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    align-items: center;
+    justify-content: flex-end;
+    
+    padding-bottom: 19px;
+`;
+
+export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(18)}px;
+    color: ${({ theme }) => theme.colors.shape};
+`;
+
+export const Category = styled.View`
+    width: 100%;
+`;
+
+export const Icon = styled(Feather)`
+
+`;
+
+export const Name = styled.Text`
+`;
