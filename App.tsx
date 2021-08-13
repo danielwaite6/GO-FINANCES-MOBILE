ebfd8f9f040d33f34cb66eaedc6818ca1cdaad0b
Daniel Waite <danielwaite6@hotmail.com>
2021-08-12 22:01:18 -0300

    DONE

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import {
@@ -9,7 +10,8 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
+//import { Register } from './src/screens/Register';
 
 export default function App() {
 
@@ -25,7 +27,9 @@ export default function App() {
   return (
     (
       <ThemeProvider theme={theme}>
-        <Register />
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
       </ThemeProvider>
     )
   );
